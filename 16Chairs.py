229b9ec4b6c312cb62d76329be6a19b3401b5c28
ajskinner7 <96786405+ajskinner7@users.noreply.github.com>
2022-07-11 08:42:03 -0500

    Add files via upload

---
+++ 16Chairs.py
@@ -0,0 +1,14 @@
+
+#16 Chairs This program asks for the user to input the names of 
+import random
+
+t1 = input("------16 chairs------ \n Type the name of the tenor in chair #1: \n(If none, type 'n')")
+#skip if no more
+t2 = input("Type the name of the tenor in chair #2:\n(If none, type 'n')")
+#t3
+#t4
+
+tenors = [t1, t2, t3, t4]
+leads = [l1, l2, l3, l4]
+baritones = [br1, br2, br3, br4]
+basses = [bs1, bs2, bs3, bs4]
