9b77eebf61efbeac2840ae802b53991f2b467609
ajskinner7 <96786405+ajskinner7@users.noreply.github.com>
2022-07-14 15:13:40 -0500

    Add files via upload
    3:13 PM 7/14

--- 16Chairs no repeater.py
+++ 16Chairs no repeater.py
@@ -26,29 +26,29 @@ def asknames(c):
     return c
 
 # function that selects a singer from each section to be in the quartet
-def drawnames(c):
-    quartet = []
+def drawnames(c, q):
     for i in c.keys():
         chosen = c[i][randrange(len(c[i]))]
-        quartet.append(chosen)
+        q.append(chosen)
         c[i].remove(chosen)
-    print(quartet)
-    return c, quartet
+    return c, q
 
 # run the program
 print('--------16 Chairs--------\n')
 
-chorus = asknames(chorus)
+asknames(chorus)
 print(chorus)   
 input('Press enter to generate a random quartet')
 
 while True:
-    drawnames(chorus)
+    quartet = []
+    drawnames(chorus, quartet)
+    print(quartet)
     input('Press enter to generate another quartet.')
     
 # To implement:
 # reset button
 # Make it look and read nicely, i.e. get plural tenses correct, no weird list formatting
 # Import a chorus from an external file, be able to delete members
-# Create UI
+# Create GUI
 # choose a singer to be in a quartet with other random singers
